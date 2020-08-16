c1787657d502f737b300fdcb41ea41947ac28f5b
ravich@CORP.AMAZONWORKSPACES.COM <ravich@CORP.AMAZONWORKSPACES.COM>
2020-08-16 18:42:47 -0400

    Add Bottom Tabs

--- App.tsx
+++ App.tsx
@@ -3,20 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons,FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons';
 
-function HomeScreen() {
+function TempScreen(props: { color: string; title: string }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center',backgroundColor:'#778899',alignItems: 'center' }}>
-      <Text style={{ color:'#F0F8FF'}}>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: props.color, alignItems: 'center' }}>
+      <Text style={{ color: '#F0F8FF' }}>{props.title}</Text>
     </View>
   );
 }
@@ -24,42 +16,63 @@ function SettingsScreen() {
 const Tab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'HOME';
 
-export default function App() { 
+export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName={INITIAL_ROUTE_NAME}
-      tabBarOptions={{
-        activeTintColor: '#1E90FF',
-      }}>
-        <Tab.Screen name="HOME" component={HomeScreen} 
-         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, size }) => <Entypo name="home" color={color} size={size} />
-        }}
+      <Tab.Navigator
+        initialRouteName={INITIAL_ROUTE_NAME}
+        tabBarOptions={{
+          activeTintColor: '#1E90FF',
+        }}>
+        <Tab.Screen
+          name="HOME"
+          component={() => <TempScreen color="#C0C0C0" title="Screen 1" />}
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="home" color={color} size={size} />
+            ),
+          }}
         />
-        <Tab.Screen name="CURRENT" component={HomeScreen} 
-         options={{
-          title: 'Current',
-          tabBarIcon: ({ color, size }) => <FontAwesome name="newspaper-o" color={color} size={size}/>
-        }}        
+        <Tab.Screen
+          name="CURRENT"
+          component={() => <TempScreen color="#87CEFA" title="Screen 2" />}
+          options={{
+            title: 'Current',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="newspaper-o" color={color} size={size} />
+            ),
+          }}
         />
-        <Tab.Screen name="PERSONAL" component={HomeScreen} 
-        options={{
-          title: 'Personal',
-          tabBarIcon: ({ color, size }) => <MaterialIcons name="business-center" color={color} size={size} />
-        }} 
+        <Tab.Screen
+          name="PERSONAL"
+          component={() => <TempScreen color="#D8BFD8" title="Screen 3" />}
+          options={{
+            title: 'Personal',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="business-center" color={color} size={size} />
+            ),
+          }}
         />
-        <Tab.Screen name="GALLERY" component={HomeScreen} 
-        options={{
-          title: 'Gallery',
-          tabBarIcon: ({ color, size }) => <MaterialIcons name="collections" color={color} size={size} />
-        }} 
+        <Tab.Screen
+          name="GALLERY"
+          component={() => <TempScreen color="#66CDAA" title="Screen 4" />}
+          options={{
+            title: 'Gallery',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="collections" color={color} size={size} />
+            ),
+          }}
         />
-        <Tab.Screen name="DOWNLOADS" component={SettingsScreen}
-        options={{
-          title: 'Downloads',
-          tabBarIcon: ({ color, size }) => <MaterialIcons name="file-download"  color={color} size={size} />
-        }} 
+        <Tab.Screen
+          name="DOWNLOADS"
+          component={() => <TempScreen color="#D2B48C" title="Screen 5" />}
+          options={{
+            title: 'Downloads',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="file-download" color={color} size={size} />
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
